02c7c93a042ecbd346a44c4af2b8605a747ae5ac
Aditya Pandey <addherbs@gmail.com>
2018-03-13 22:07:53 -0500

    reverse Integrated

--- linked_list.c
+++ linked_list.c
@@ -438,3 +438,20 @@ list sublist(list A, list pos_list) {
 	
     return newLList;
 }
+
+
+void reverse(list the_list) {
+    if (listIsNULL(the_list) == 1) {
+        return;
+    }
+
+    link current = the_list->first;
+    link previous = NULL;
+    while (current != NULL) {
+        link temp = current->next;
+        current->next = previous;
+        previous = current;
+        current = temp;
+    }
+    the_list->first = previous;
+}
